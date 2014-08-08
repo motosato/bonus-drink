4f029726c9aeb8ab890ac009c30614174a3d7ad1
motosato <motonori.sato@hyakuren.org>
2014-08-08 22:49:38 +0900

    fix:total_count_for method

--- bonus_drink.rb
+++ bonus_drink.rb
@@ -1,5 +1,18 @@
 class BonusDrink
   def self.total_count_for(amount)
-    0
+    # a[n] = 1,2,4,5,7...
+    # b[n] = 1,2,1,2,...
+    # c[n] = 1,-1,1,-1...
+    # となる階差数列を解けばよいはずで、
+    # b[n] = b1 + Σc[k]  (k=1,n-1)
+    #      = 1 + Σ(1 * (-1)**(k-1))  (k=1,n-1)
+    # となるが、解法に苦しみggr
+    # http://okwave.jp/qa/q3154207.html
+    # 自然数であるため、0が考慮されてない。辻褄あわせのabsを使用
+    amount + (amount-1).abs/2
   end
+end
+
+(0..100).each do |amount|
+  print("amount:#{amount}, total:#{BonusDrink::total_count_for(amount)}\n")
 end
